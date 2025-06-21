188f35596bb53b1e48f62935a76d339b1d1f3da5
raj1-bit <shivankrc666@gmail.com>
2025-06-21 16:25:07 +0000

    fff

---
+++ 02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as never version
+//alert(3+3)// we are using node js not broser
+let name = "raj"
+let age= "19"
+let isLoggedin = false
+/*number =>2 to the poerr 53
+bigint
+string=>"" 
+bolean=> true/false
+nell => stand alon value
+undefined =>
+    symbol=>unique
+object*/
+
+
+console.log(typeof"raj");
+console.log(typeof"age");
+console.log(typeof undefined);// undefine
+console.log(typeof null);//object
