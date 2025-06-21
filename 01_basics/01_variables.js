e883d8454aa3a9c237b021f012c9134f5e4130da
raj1-bit <shivankrc666@gmail.com>
2025-06-21 16:02:25 +0000

    ttt

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =12234
+let accountEmail = "raj@google.com"
+var accountPassword = "12345"
+accountcity ="bhopal"
+//accountId = 2 wednot allo
+ let accountState;
+accountEmail  = "rajQ@.com"
+accountPassword = "1233344"
+accountcity =" mohi"
+console.log(accountId);
+
+
+
+/*prefer not touse var
+ because  of issu in block scope and functional scope */
+console.table([accountId , accountEmail , accountPassword ,accountcity , accountState ])
